534979643ab579cee3789c97366efd605c9afb06
IgorSamagalskyi <batelgeizea@gmail.com>
2023-04-10 01:13:59 +0300

    commit  2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
-
+import Users from "./components/users/Users";
 // let hello = "Hello All";  глобальна функція
 
 function App() {
-  return <div className="products"></div>;
+  return (
+    <div className="products">
+      <Users />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/users/Users.js
@@ -0,0 +1,5 @@
+function Users() {
+  return <div>Woc'</div>;
+}
+
+export default Users;
