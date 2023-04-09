75beee8c974d1e7b43acb1f45efa5f29d084ef55
IgorSamagalskyi <batelgeizea@gmail.com>
2023-04-10 02:00:04 +0300

    create folder Users and User

---
+++ src/components/user/User.js
@@ -0,0 +1,9 @@
+function User({ userItem }) {
+  return (
+    <div>
+      {userItem.id}' '{userItem.name}
+    </div>
+  );
+}
+
+export default User;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,5 +1,14 @@
+import { usersList } from "../../data-file";
+import User from "../user/User";
+
 function Users() {
-  return <div>Woc'</div>;
+  return (
+    <div>
+      {usersList.map((userItem) => (
+        <User key={userItem.id} userItem={userItem} />
+      ))}
+    </div>
+  );
 }
 
 export default Users;
