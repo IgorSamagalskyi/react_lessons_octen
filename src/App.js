f206d1ccd7c8bf3f361de19f2d23f3b31d747df5
IgorSamagalskyi <batelgeizea@gmail.com>
2023-04-10 00:56:34 +0300

    Lesson 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
-import Product from "./components/product/Product";
+
 // let hello = "Hello All";  глобальна функція
 
 function App() {
-  return (
-    <div className="products">
-      <Product title={"milk"} price={"20uah"} />
-      <Product title={"jus"} price={"40uah"} />
-    </div>
-  );
+  return <div className="products"></div>;
 }
 
 export default App;
