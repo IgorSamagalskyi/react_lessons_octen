348767d17bab3d6adc7def4dd7877ab141fa51ff
IgorSamagalskyi <batelgeizea@gmail.com>
2023-04-10 11:47:35 +0300

    install axios

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,7 +1,15 @@
+let url = "https://jsonplaceholder.typicode.com/users";
+
 const getUsers = () => {
-  return fetch("https://jsonplaceholder.typicode.com/users").then((value) =>
-    value.json()
-  );
+  return fetch(url).then((value) => value.json());
 };
 
-export { getUsers };
+const getUser = async (id) => {
+  if (+id < 0) {
+    throw new Error("мусить бути id! id не має");
+  }
+
+  return await fetch(url + "/" + id).then((value) => value.json());
+};
+
+export { getUsers, getUser };
